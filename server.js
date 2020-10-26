e9b0959d8c820ff87b3a1ecf4cdfa4626256f8a5
Ivan Ip <m@lifeho.me>
2020-10-26 20:32:22 +0800

    remove babel and dotenv

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// Summon env variables
-require('dotenv').config()
-
 import Telegraf from "telegraf"
 const instance = new Telegraf(process.env.BOT_TOKEN)
 const serviceMessageFilter = ["new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo", "group_chat_created", "migrate_to_chat_id", "supergroup_chat_created", "channel_chat_created", "migrate_from_chat_id", "pinned_message"]
