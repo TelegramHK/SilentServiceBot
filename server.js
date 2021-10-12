81af4d489525cbbd9d30ea3c1779dab7c50066e0
Ivan Ip <m@lifeho.me>
2021-10-12 15:47:50 +0800

    🪠 patch: properly catch error without crashing the script

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const instance = new Telegraf(process.env.BOT_TOKEN)
 const serviceMessageFilter = ["new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo", "group_chat_created", "migrate_to_chat_id", "supergroup_chat_created", "channel_chat_created", "migrate_from_chat_id", "pinned_message"]
 
 instance.on(serviceMessageFilter, ctx=>{
-  ctx.deleteMessage()
-})
-
-// Instance Error Catching
-instance.catch((err) => {
-  console.log(err) // TODO: Refactor and produce a better error catcher module
+  ctx.deleteMessage().catch(err => console.log(err))
 })
 
 instance.launch({
